2c3e778c07a5c6f1eb7e345f8c428a0bf8e88a37
Catriel Guillen <30703501+pyscriptbug@users.noreply.github.com>
2023-11-20 14:35:09 +0000

    fix: Import root router

--- src/expressjs/index.js
+++ src/expressjs/index.js
@@ -1,5 +1,6 @@
 import { initExpress, registerRouter, startServer } from "./app.js";
 import { catsRouter } from "./routes/cats.router.js";
+import { rootRouter } from "./routes/root.router.js";
 
 async function main() {
     initExpress();
